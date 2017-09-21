2f7f9357253b46f131d768f57e70564546122dd1
avalberg <valberg@ualberta.ca>
2017-09-21 13:35:26 -0600

    Added Week 2 & 3 code examples

---
+++ Week 2 Hello Example/Sept15Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sept15Example
+{
+    class Program
+    {
+        private static string _FirstName;
+        public static string LastName;
+
+        public static string FirstName
+        {
+            get { return _FirstName; }
+            set { _FirstName = value; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first name: ");
+            FirstName = Console.ReadLine();
+
+            Console.Write("Enter last name: ");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine(Hello(FirstName, LastName));
+        }
+
+        public static string Hello(string firstname, string lastname)
+        {
+            string toReturn;
+            toReturn = "Hello " + firstname + " " + lastname + "!";
+            return toReturn;
+        }
+    }
+}
---
+++ Week 3 Celsius Fahrenheit Example/Sept21CFExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sept21CFExample
+{
+    class Program
+    {
+        // We're going to make a basic program together that uses methods
+        // to convert celsius to fahrenheit, and vice versa!
+
+        // We need to use all 3 of the variables provided. We can make more
+        // if we need to. We need to use both methods provided.
+
+        // What writes to our console should be formatted EXACTLY like this: 
+        // Enter a value in celsius: 12
+        // 12C is 53.6F
+        // Enter a value in fahrenheit: 40
+        // 40F is 4.4444444444C
+        // Press any key to continue...
+
+        // PS - here are the conversion formulas:
+            // C to F: C * 1.8 + 32
+            // F to C: (F - 32) / 1.8
+
+        static void Main(string[] args)
+        {
+            double userValueCelsius;
+            double userValueFahrenheit;
+            double fValueConvertedToC;
+
+            Console.Write("Enter a value in celsius: ");
+            userValueCelsius = double.Parse(Console.ReadLine());
+
+            CelsiusToFahrenheit(userValueCelsius);
+
+            Console.Write("Enter a value in fahrenheit: ");
+            userValueFahrenheit = double.Parse(Console.ReadLine());
+            fValueConvertedToC = FahrenheitToCelsius(userValueFahrenheit);
+            Console.WriteLine(userValueFahrenheit + "F is " + fValueConvertedToC + "C");
+        }
+
+        public static void CelsiusToFahrenheit(double userValue)
+        {
+            Console.WriteLine(userValue + "C is " + (userValue * 1.8 + 32) + "F");
+        }
+
+        public static double FahrenheitToCelsius(double userValue)
+        {
+            return (userValue - 32) / 1.8;
+        }
+    }
+}
