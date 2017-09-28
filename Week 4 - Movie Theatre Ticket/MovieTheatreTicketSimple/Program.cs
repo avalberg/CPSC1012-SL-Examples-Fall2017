582a7b5e714abc014c0e47eb0b1390c555b8d2d9
avalberg <valberg@ualberta.ca>
2017-09-28 15:09:48 -0600

    Finished the program

--- Week 4 - Movie Theatre Ticket/MovieTheatreTicketSimple/Program.cs
+++ Week 4 - Movie Theatre Ticket/MovieTheatreTicketSimple/Program.cs
@@ -10,15 +10,19 @@ namespace MovieTheatreTicketSimple
     {
         static void Main(string[] args)
         {
+            // Declaring our variables
             int age;
             double total = 0;
-            char userChoice;
+            char userSnack;
 
+            // Writing the title
             Console.WriteLine("Movie Theatre\n-------------");
             Console.Write("How old are you? ");
 
+            // Taking in user's age
             age = int.Parse(Console.ReadLine());
 
+            // If/else to determine ticket price based on age
             if (age < 13)
             {
                 Console.WriteLine("\nYour ticket is 8$.");
@@ -34,6 +38,33 @@ namespace MovieTheatreTicketSimple
                 Console.WriteLine("\nYour ticket is 12$.");
                 total += 12;
             }
+
+            // Writing snack options
+            Console.Write("\n\nPick a snack: \nA) Popcorn - 5$\nB) Skittles - 2$\n"
+                            + "C) Cotton candy - 3$\nEnter your snack choice: ");
+
+            // Taking in user's snack choice
+            userSnack = char.Parse(Console.ReadLine().ToUpper());
+
+            // Switch to figure out user's snack choice
+            switch (userSnack)
+            {
+                case 'A':
+                    total += 5;
+                    break;
+                case 'B':
+                    total += 2;
+                    break;
+                case 'C':
+                    total += 3;
+                    break;
+                default:
+                    break;
+            }
+
+            // Writing the bill total
+            Console.WriteLine("\n-------------------------"
+                + $"\nYour total bill is {total:C}\n\n");
         }
     }
 }
