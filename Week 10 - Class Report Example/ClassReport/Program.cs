02404440fa4e32094e90183762059ba0065da1ec
avalberg <valberg@ualberta.ca>
2017-11-07 10:04:57 -0700

    Added week 10 example for use in class

---
+++ Week 10 - Class Report Example/ClassReport/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numStudents;
+            string className;
+            double[] grades = new double[25];
+
+            //TODO: create loopable menu with 6 options (set up start over, exit & default)
+            //TODO: create 3 paths of logic (to be put in switch) - enter class name, # of students, grades
+            //TODO: create 4th path of logic (to be put in switch) - view class report, 
+                // which includes all the calculations and produces the class report output
+        }
+    }
+}
