2de8627ed4ff9cb749f5e54164d3beb735e9096d
avalberg <valberg@ualberta.ca>
2017-11-10 12:48:56 -0700

    Added Quiz Problems, updated Class Report Example

---
+++ QuizProblems/QuizProblems/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Multiplication table
+            for (int row = 1; row <= 9; row++)
+            {
+                for (int col = 1; col <= 9; col++)
+                {
+                    Console.Write("{0,3}", col);
+                }
+                Console.WriteLine();
+                //}
+
+                // Take an avg of numbers provided by the user
+                double avg = 0;
+            string input = "";
+            int count;
+            char again = 'Y';
+            do
+            {
+                input = "";
+                avg = 0;
+                for (count = 1; input != "="; count++)
+                {
+                    Console.Write("Enter a mark: ");
+                    input = Console.ReadLine();
+                    if (input != "=")
+                        avg += double.Parse(input);
+                }
+                avg /= count;
+                Console.Write("The average is {0}\n", avg);
+                Console.Write("Try again? Y/N");
+                again = char.Parse(Console.ReadLine().ToUpper());
+            } while (again == 'Y');
+        }
+    }
+}
--- Week 10 - Class Report Example/ClassReport/Program.cs
+++ Week 10 - Class Report Example/ClassReport/Program.cs
@@ -10,11 +10,49 @@ namespace ClassReport
     {
         static void Main(string[] args)
         {
+            int userInput;
             int numStudents;
             string className;
             double[] grades = new double[25];
 
-            //TODO: create loopable menu with 6 options (set up start over, exit & default)
+            do
+            {
+                Console.WriteLine("1) Enter class name");
+                Console.WriteLine("2) Enter number of students");
+                Console.WriteLine("3) Enter student grades");
+                Console.WriteLine("4) View Class Report");
+                Console.WriteLine("5) Start Over");
+                Console.WriteLine("6) Exit");
+                Console.Write("Your choice: ");
+                while (!int.TryParse(Console.ReadLine(), out userInput))
+                {
+                    Console.Write("Invalid choice. Try again: ");
+                }
+                switch (userInput)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        numStudents = 0;
+                        className = "";
+                        for (int i = 0; i < grades.Length; i++)
+                        {
+                            grades[i] = 0;
+                        }
+                        break;
+                    case 6:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            } while (userInput != 6 || userInput < 0);
             //TODO: create 3 paths of logic (to be put in switch) - enter class name, # of students, grades
             //TODO: create 4th path of logic (to be put in switch) - view class report, 
                 // which includes all the calculations and produces the class report output
