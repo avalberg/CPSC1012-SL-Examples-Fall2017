d515f7c19043c54d330e85f6ea69156d175e87be
avalberg <valberg@ualberta.ca>
2017-11-14 08:40:54 -0700

    Class Report finished - ready for methods

--- Week 10 - Class Report Example/ClassReport/Program.cs
+++ Week 10 - Class Report Example/ClassReport/Program.cs
@@ -11,8 +11,8 @@ namespace ClassReport
         static void Main(string[] args)
         {
             int userInput;
-            int numStudents = 0;
-            string className = "";
+            int numStudents = 2;
+            string className = "classname";
             double[] grades = new double[25];
 
             do
@@ -36,11 +36,15 @@ namespace ClassReport
                             Console.Write("Class name must be minimum 1 character, maximum 30 characters. Try again: ");
                             className = Console.ReadLine();
                         }
+                        Console.Clear();
+                        Console.Write("Class name entered!\n\n");
                         break;
                     case 2:
                         Console.Write("Enter number of students: ");
-                        while(!int.TryParse(Console.ReadLine(), out numStudents) || numStudents == 0 || numStudents > 25)
+                        while (!int.TryParse(Console.ReadLine(), out numStudents) || numStudents == 0 || numStudents > 25)
                             Console.Write("Invalid choice. Minimum number of students is 1, maximum is 25. Try again: ");
+                        Console.Clear();
+                        Console.Write("Number of students entered!\n\n");
                         break;
                     case 3:
                         if (numStudents == 0)
@@ -57,19 +61,38 @@ namespace ClassReport
                                 Console.Write("Invalid grade - must be between 0 and 100 (inclusive). Try again: ");
                             grades[count] = localGrade;
                         }
+                        Console.Clear();
+                        Console.Write("Grades entered successfully!\n\n");
                         break;
                     case 4:
+                        Console.Clear();
                         if (className == "")
-                            Console.Write("You need a class name to view the report. You are now being returned to the main menu.\n");
+                            Console.Write("You need a class name to view the report.\n\n");
                         else if (numStudents == 0)
-                            Console.Write("You need a class name to view the report. You are now being returned to the main menu.\n");
+                            Console.Write("You need to have entered number of students to view the report.\n\n");
                         else
                         {
+                            double avg = 0;
+                            for (int i = 0; i < numStudents; i++)
+                                avg += grades[i];
+                            avg /= numStudents;
                             Console.Write(className);
                             Console.Write("\n------------------------------");
                             Console.Write("\n{0,-25}{1,5}", "Number of students: ", numStudents);
-                            Console.Write("\n{0,-25}", "Student grades: ");
-
+                            Console.Write("\n{0,-17}", "Student grades: ");
+                            for (int count = 0; count < numStudents; count++)
+                                Console.Write(grades[count] + " ");
+                            Console.Write("\n{0,-24}{1,5}{2,1}\n", "Grade Average: ", avg, "%");
+                            Console.Write("\n\nDo you want to go back to the menu? Press any key to return to the menu, or Esc to Exit.\n\n");
+                            if (Console.ReadKey().Key == ConsoleKey.Escape)
+                            {
+                                Console.WriteLine("Bye!");
+                                Environment.Exit(0);
+                            }
+                            else
+                            {
+                                Console.Clear();
+                            }
                         }
                         break;
                     case 5:
@@ -79,6 +102,8 @@ namespace ClassReport
                         }
                         numStudents = 0;
                         className = "";
+                        Console.Clear();
+                        Console.Write("All fields successfully reset.\n\n");
                         break;
                     case 6:
                         Environment.Exit(0);
@@ -87,10 +112,9 @@ namespace ClassReport
                         Console.Write("Please pick an option between 1 and 6.\n");
                         break;
                 }
-            } while (userInput != 6 || userInput < 0);
-            //TODO: create 3 paths of logic (to be put in switch) - enter class name, # of students, grades
-            //TODO: create 4th path of logic (to be put in switch) - view class report, 
-                // which includes all the calculations and produces the class report output
+            } while (userInput != 6);
+            //TODO: move switch components into methods
+            //TODO: move switch (menu) into a method
         }
     }
 }
