6dec991160a7f02cf0b089fdfe7d8d39873ddd1d
avalberg <valberg@ualberta.ca>
2017-11-16 11:41:19 -0700

    Finished Class Report example - now includes methods

--- Week 10 - Class Report Example/ClassReport/Program.cs
+++ Week 10 - Class Report Example/ClassReport/Program.cs
@@ -17,34 +17,16 @@ namespace ClassReport
 
             do
             {
-                Console.WriteLine("1) Enter class name");
-                Console.WriteLine("2) Enter number of students");
-                Console.WriteLine("3) Enter student grades");
-                Console.WriteLine("4) View Class Report");
-                Console.WriteLine("5) Start Over");
-                Console.WriteLine("6) Exit");
-                Console.Write("Your choice: ");
+                DisplayMenu();
                 while (!int.TryParse(Console.ReadLine(), out userInput))
                     Console.Write("Invalid choice. Try again: ");
                 switch (userInput)
                 {
                     case 1:
-                        Console.Write("Enter the class name: ");
-                        className = Console.ReadLine();
-                        while (className.Length > 30 || className.Length < 1)
-                        {
-                            Console.Write("Class name must be minimum 1 character, maximum 30 characters. Try again: ");
-                            className = Console.ReadLine();
-                        }
-                        Console.Clear();
-                        Console.Write("Class name entered!\n\n");
+                        className = GetClassName();
                         break;
                     case 2:
-                        Console.Write("Enter number of students: ");
-                        while (!int.TryParse(Console.ReadLine(), out numStudents) || numStudents == 0 || numStudents > 25)
-                            Console.Write("Invalid choice. Minimum number of students is 1, maximum is 25. Try again: ");
-                        Console.Clear();
-                        Console.Write("Number of students entered!\n\n");
+                        numStudents = GetNumStudents();
                         break;
                     case 3:
                         if (numStudents == 0)
@@ -52,17 +34,7 @@ namespace ClassReport
                             Console.Write("You must provide a number of students before you can enter grades.\nYou will now be returned to the main menu.\n");
                             break;
                         }
-                        Console.Write("Enter a grade for each student (there are {0}).\n", numStudents);
-                        double localGrade;
-                        for (int count = 0; count < numStudents; count++)
-                        {
-                            Console.Write("Student {0}'s grade: ", count + 1);
-                            while (!double.TryParse(Console.ReadLine(), out localGrade) || localGrade < 0 || localGrade > 100)
-                                Console.Write("Invalid grade - must be between 0 and 100 (inclusive). Try again: ");
-                            grades[count] = localGrade;
-                        }
-                        Console.Clear();
-                        Console.Write("Grades entered successfully!\n\n");
+                        GetStudentGrades(numStudents, grades);
                         break;
                     case 4:
                         Console.Clear();
@@ -72,38 +44,11 @@ namespace ClassReport
                             Console.Write("You need to have entered number of students to view the report.\n\n");
                         else
                         {
-                            double avg = 0;
-                            for (int i = 0; i < numStudents; i++)
-                                avg += grades[i];
-                            avg /= numStudents;
-                            Console.Write(className);
-                            Console.Write("\n------------------------------");
-                            Console.Write("\n{0,-25}{1,5}", "Number of students: ", numStudents);
-                            Console.Write("\n{0,-17}", "Student grades: ");
-                            for (int count = 0; count < numStudents; count++)
-                                Console.Write(grades[count] + " ");
-                            Console.Write("\n{0,-24}{1,5}{2,1}\n", "Grade Average: ", avg, "%");
-                            Console.Write("\n\nDo you want to go back to the menu? Press any key to return to the menu, or Esc to Exit.\n\n");
-                            if (Console.ReadKey().Key == ConsoleKey.Escape)
-                            {
-                                Console.WriteLine("Bye!");
-                                Environment.Exit(0);
-                            }
-                            else
-                            {
-                                Console.Clear();
-                            }
+                            DisplayClassReport(className, numStudents, grades);
                         }
                         break;
                     case 5:
-                        for (int i = 0; i < numStudents; i++)
-                        {
-                            grades[i] = 0;
-                        }
-                        numStudents = 0;
-                        className = "";
-                        Console.Clear();
-                        Console.Write("All fields successfully reset.\n\n");
+                        StartOver(ref className, ref numStudents, grades);
                         break;
                     case 6:
                         Environment.Exit(0);
@@ -113,8 +58,95 @@ namespace ClassReport
                         break;
                 }
             } while (userInput != 6);
-            //TODO: move switch components into methods (1-5, not 6)
-            //TODO: move switch (menu) into a method
+        }
+
+        public static void StartOver(ref string className, ref int numStudents, double[] grades)
+        {
+            for (int i = 0; i < numStudents; i++)
+            {
+                grades[i] = 0;
+            }
+            numStudents = 0;
+            className = "";
+            Console.Clear();
+            Console.Write("All fields successfully reset.\n\n");
+        }
+
+        public static void DisplayClassReport(string className, int numStudents, double[] grades)
+        {
+            double avg = 0;
+            for (int i = 0; i < numStudents; i++)
+                avg += grades[i];
+            avg /= numStudents;
+            Console.Write(className);
+            Console.Write("\n------------------------------");
+            Console.Write("\n{0,-25}{1,5}", "Number of students: ", numStudents);
+            Console.Write("\n{0,-17}", "Student grades: ");
+            for (int count = 0; count < numStudents; count++)
+                Console.Write(grades[count] + " ");
+            Console.Write("\n{0,-24}{1,5}{2,1}\n", "Grade Average: ", avg, "%");
+            Console.Write("\n\nDo you want to go back to the menu? Press any key to return to the menu, or Esc to Exit.\n\n");
+            if (Console.ReadKey().Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine("Bye!");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.Clear();
+            }
+
+        }
+
+        public static void GetStudentGrades(int numStudents, double[] grades)
+        {
+            Console.Write("Enter a grade for each student (there are {0}).\n", numStudents);
+            double localGrade;
+            for (int count = 0; count < numStudents; count++)
+            {
+                Console.Write("Student {0}'s grade: ", count + 1);
+                while (!double.TryParse(Console.ReadLine(), out localGrade) || localGrade < 0 || localGrade > 100)
+                    Console.Write("Invalid grade - must be between 0 and 100 (inclusive). Try again: ");
+                grades[count] = localGrade;
+            }
+            Console.Clear();
+            Console.Write("Grades entered successfully!\n\n");
+        }
+
+        public static int GetNumStudents()
+        {
+            int localNumStudents;
+            Console.Write("Enter number of students: ");
+            while (!int.TryParse(Console.ReadLine(), out localNumStudents) || localNumStudents <= 0 || localNumStudents > 25)
+                Console.Write("Invalid choice. Minimum number of students is 1, maximum is 25. Try again: ");
+            Console.Clear();
+            Console.Write("Number of students entered!\n\n");
+            return localNumStudents;
+        }
+
+        public static string GetClassName()
+        {
+            Console.Write("Enter the class name: ");
+            string localClassName = Console.ReadLine();
+            while (localClassName.Length > 30 || localClassName.Length < 1)
+            {
+                Console.Write("Class name must be minimum 1 character, maximum 30 characters. Try again: ");
+                localClassName = Console.ReadLine();
+            }
+            Console.Clear();
+            Console.Write("Class name entered!\n\n");
+            return localClassName;
+        }
+
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("1) Enter class name");
+            Console.WriteLine("2) Enter number of students");
+            Console.WriteLine("3) Enter student grades");
+            Console.WriteLine("4) View Class Report");
+            Console.WriteLine("5) Start Over");
+            Console.WriteLine("6) Exit");
+            Console.Write("Your choice: ");
         }
     }
 }
