65126db9414ef0e6f06ad449cd0320a0d3b85088
avalberg <valberg@ualberta.ca>
2017-11-24 12:51:36 -0700

    Added an array to hold multiple Chairs; showed method overloading

--- Week 12 Objects - Chair Example/ChairExample/Program.cs
+++ Week 12 Objects - Chair Example/ChairExample/Program.cs
@@ -10,54 +10,62 @@ namespace ChairExample
     {
         static void Main(string[] args)
         {
+            Chair[] chairs = new Chair[3];
             Chair.chairType type;
             Chair.primaryMaterial material;
             string colour, name;
-            int selection;
+            int selection, chairsCount = 0;
             
             // title
             Console.Write("Inventory Management\n--------------------");
+            do
+            {
+                // getting user input for chair type and parsing it
+                Console.Write("\nSelect a chair type: \n1) Rolling\n2) Stationary\n3) Stool\nYour choice: ");
+                while (!int.TryParse(Console.ReadLine(), out selection) || (selection != 1 && selection != 2 && selection != 3))
+                    Console.Write("Invalid selection. Your choice: ");
 
-            // getting user input for chair type and parsing it
-            Console.Write("\nSelect a chair type: \n1) Rolling\n2) Stationary\n3) Stool\nYour choice: ");
-            while (!int.TryParse(Console.ReadLine(), out selection) || (selection != 1 && selection != 2 && selection != 3))
-                Console.Write("Invalid selection. Your choice: ");
+                // setting chair type based on user input
+                if (selection == 1)
+                    type = Chair.chairType.rolling;
+                else if (selection == 2)
+                    type = Chair.chairType.stationary;
+                else
+                    type = Chair.chairType.stool;
 
-            // setting chair type based on user input
-            if (selection == 1)
-                type = Chair.chairType.rolling;
-            else if (selection == 2)
-                type = Chair.chairType.stationary;
-            else
-                type = Chair.chairType.stool;
+                // getting user input for chair material and parsing it
+                Console.Write("Select a chair material: \n1) Wood\n2) Cloth\n3) Plastic\nYour choice: ");
+                while (!int.TryParse(Console.ReadLine(), out selection) || (selection != 1 && selection != 2 && selection != 3))
+                    Console.Write("Invalid selection. Your choice: ");
 
-            // getting user input for chair material and parsing it
-            Console.Write("Select a chair material: \n1) Wood\n2) Cloth\n3) Plastic\nYour choice: ");
-            while (!int.TryParse(Console.ReadLine(), out selection) || (selection != 1 && selection != 2 && selection != 3))
-                Console.Write("Invalid selection. Your choice: ");
+                // setting chair material based on user input
+                if (selection == 1)
+                    material = Chair.primaryMaterial.wood;
+                else if (selection == 2)
+                    material = Chair.primaryMaterial.cloth;
+                else
+                    material = Chair.primaryMaterial.plastic;
 
-            // setting chair material based on user input
-            if (selection == 1)
-                material = Chair.primaryMaterial.wood;
-            else if (selection == 2)
-                material = Chair.primaryMaterial.cloth;
-            else
-                material = Chair.primaryMaterial.plastic;
+                // getting user input for chair colour (with no real validation)
+                Console.Write("Enter a chair colour: ");
+                colour = Console.ReadLine();
 
-            // getting user input for chair colour (with no real validation)
-            Console.Write("Enter a chair colour: ");
-            colour = Console.ReadLine();
+                // getting user input for chair name (again, not really validating)
+                Console.Write("Enter a chair name: ");
+                name = Console.ReadLine();
 
-            // getting user input for chair name (again, not really validating)
-            Console.Write("Enter a chair name: ");
-            name = Console.ReadLine();
-
-            // now, the fun stuff: we make the chair!
-            // we're passing in the parameters we just took so that the constructor can use them
-            Chair userChair = new Chair(type,material,colour,name);
-
-            // because we now have a chair (and the Chair class has a method in it!), we can call our chair's method
-            userChair.DisplayChairAttributes();
+                // now, the fun stuff: we make the chair!
+                // we're passing in the parameters we just took so that the constructor can use them
+                Chair userChair = new Chair(type, material, colour, name);
+                chairs[chairsCount] = userChair;
+                chairsCount++;
+            } while (chairsCount <= 2);
+            // because we now have chairs in our array, we can display their attributes!
+            foreach (Chair chair in chairs)
+                chair.DisplayChairAttributes();
+            // OR
+            for (int i = 0; i < chairs.Length; i++)
+                chairs[i].DisplayChairAttributes();
         }
     }
 }
